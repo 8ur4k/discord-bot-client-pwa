4eef13cb5c3ea52c6bf2ed9f3bf7976df95cb2e7
Burak <nobleworms@hotmail.com>
2021-05-23 08:23:27 +0300

    Commit

--- public/js/index.js
+++ public/js/index.js
@@ -5,6 +5,9 @@ socket.on("alert-mode", (reason) => {
   console.log(reason);
 });
 
+// DENEME
+console.log("Hello world");
+
 button.addEventListener("click", (e) => {
   e.preventDefault();
   socket.emit("alert-mode");
